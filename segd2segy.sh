85406e890a3ff59ddddb9d57b5111075c53804b1
Débora Everly <92762875+deboraeverlyab@users.noreply.github.com>
2024-03-05 16:33:19 -0300

    atualização do tipo de arquivo e inserção de remoção de lista temporária

--- segd2segy.sh
+++ segd2segy.sh
@@ -2,7 +2,7 @@
 
 ls -v *.sgd > /tmp/lista
 
-rm /tmp/lista.su
+rm /tmp/lista.su 2> /dev/null 
 
 cat /tmp/lista | while read line 
 do
@@ -11,3 +11,5 @@ segdread tape=$line use_stdio=1 >> /tmp/lista.su
 done
 
 cat /tmp/lista.su | segyhdrs | segywrite tape=$1.sgy
+
+rm /tmp/lista.su
